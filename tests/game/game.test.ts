0afcdd883689e7bf108dc626d9f8679bbac6cc42
Maycon Carlete <maycon.carlete@hotmail.com>
2021-05-10 21:02:22 -0300

    test: ensure Game have frame with 9 positions

--- tests/game/game.test.ts
+++ tests/game/game.test.ts
@@ -1,8 +1,19 @@
 import { Game } from "../../src/game/game"
 
+
+const makeSut = ():Game => {
+    const frame = [1,2,3,4,5,6,7,8,9]
+    const game = new Game(frame)
+    return game
+}
+
 describe('Game Class', () => {
-    test('Should crete an instance of Game', () => {
-        const sut = new Game()
+    test('Should create an instance of Game', () => {
+        const sut = makeSut()
         expect(sut).toBeInstanceOf(Game)
+    })
+    test('Should create an array of position to tic tac toe', () => {
+        const sut = makeSut()
+        expect(sut.getFrame.length).toBe(9)
     })
 })
