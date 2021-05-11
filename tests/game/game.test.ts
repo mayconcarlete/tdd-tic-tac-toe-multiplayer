f011c8104c45890786530129c33b0b2966922131
Maycon Carlete <maycon.carlete@hotmail.com>
2021-05-10 21:27:27 -0300

    test: ensure Game class has a frame with element of type Frame

--- tests/game/game.test.ts
+++ tests/game/game.test.ts
@@ -1,9 +1,22 @@
 import { Game } from "../../src/game/game"
+import { Frame } from "../../src/types/frame"
 
 
-const makeSut = ():Game => {
-    const frame = [1,2,3,4,5,6,7,8,9]
-    const game = new Game(frame)
+const makeSut = ():Game => { 
+    const frame:Frame[] = []
+    // for(let i = 0; i<9;i++) frame.push(new Frame(i))
+   const game = new Game([
+    new Frame(0),
+    new Frame(1),
+    new Frame(2),
+    new Frame(3),
+    new Frame(4),
+    new Frame(5),
+    new Frame(6),
+    new Frame(7),
+    new Frame(8)
+   ])
+
     return game
 }
 
@@ -15,5 +28,9 @@ describe('Game Class', () => {
     test('Should create an array of position to tic tac toe', () => {
         const sut = makeSut()
         expect(sut.getFrame.length).toBe(9)
+    })
+    test('Should ensure that frame array is from type Frame', () => {
+        const sut = makeSut()
+        expect(sut.getFrame[0]).toBeInstanceOf(Frame)
     })
 })
