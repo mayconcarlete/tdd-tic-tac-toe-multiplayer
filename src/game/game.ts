201cff18d832b9ae6e0aeb788f356a6c8b5cee2e
Maycon Carlete <maycon.carlete@hotmail.com>
2021-05-11 21:00:31 -0300

    feat: ensure Game has player two when game starts

--- src/game/game.ts
+++ src/game/game.ts
@@ -4,12 +4,16 @@ import { Player } from "../types/player";
 export class Game {
     constructor(
         private readonly frame:Frame[],
-        private readonly playerOne: Player
+        private readonly playerOne: Player,
+        private readonly playerTwo: Player
     ){}
     get getFrame(){
         return this.frame
     }
     get getPlayerOne(){
         return this.playerOne
+    }
+    get getPlayerTwo(){
+        return this.playerTwo
     }
 }
