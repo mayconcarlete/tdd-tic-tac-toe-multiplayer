488876b3c384200ac065a5777222ae4391b3b839
Maycon Carlete <maycon.carlete@gmail.com>
2021-09-07 11:22:41 -0300

    feat: ensure Game returns victory when player won in first vertical lane

--- src/game/game.ts
+++ src/game/game.ts
@@ -32,6 +32,8 @@ export class Game {
         if(this.historyPlays[0] !== -1 && this.historyPlays[0] === this.historyPlays[1] && this.historyPlays[0] === this.historyPlays[2]) return true
         if(this.historyPlays[3] !== -1 && this.historyPlays[3] === this.historyPlays[4] && this.historyPlays[3] === this.historyPlays[5]) return true
         if(this.historyPlays[6] !== -1 && this.historyPlays[6] === this.historyPlays[7] && this.historyPlays[6] === this.historyPlays[8]) return true
+        if(this.historyPlays[0] !== -1 && this.historyPlays[0] === this.historyPlays[3] && this.historyPlays[0] === this.historyPlays[6]) return true
+        
         return false
     }
 }
--- tests/game/constants.ts
+++ tests/game/constants.ts
@@ -1,3 +1,5 @@
 export const horizontalFirstLine: number[] = [0, 0, -1, -1, -1, -1, -1, -1, -1] 
 export const horizontalSecondLine: number[] = [-1, -1, -1, 0, 0, -1, -1, -1, -1] 
 export const horizontalThirdLine: number[] = [-1, -1, -1, -1, -1, -1, 0, 0, -1] 
+
+export const verticalFirstLane: number[] = [1, -1, -1, 1, -1, -1, -1, -1, -1]
