1013f7008491adbb1baa6fbd270acf87ee5200f0
Maycon Carlete <maycon.carlete@gmail.com>
2021-09-07 11:53:17 -0300

    feat: ensure Game returns victory when player won in diagonal one

--- src/game/game.ts
+++ src/game/game.ts
@@ -29,12 +29,16 @@ export class Game {
     }
 
     isHorizontalVictory(): boolean{
+        // test for horizontal lanes
         if(this.historyPlays[0] !== -1 && this.historyPlays[0] === this.historyPlays[1] && this.historyPlays[0] === this.historyPlays[2]) return true
         if(this.historyPlays[3] !== -1 && this.historyPlays[3] === this.historyPlays[4] && this.historyPlays[3] === this.historyPlays[5]) return true
         if(this.historyPlays[6] !== -1 && this.historyPlays[6] === this.historyPlays[7] && this.historyPlays[6] === this.historyPlays[8]) return true
+        // test for vertical lanes
         if(this.historyPlays[0] !== -1 && this.historyPlays[0] === this.historyPlays[3] && this.historyPlays[0] === this.historyPlays[6]) return true
         if(this.historyPlays[1] !== -1 && this.historyPlays[1] === this.historyPlays[4] && this.historyPlays[1] === this.historyPlays[7]) return true
         if(this.historyPlays[2] !== -1 && this.historyPlays[2] === this.historyPlays[5] && this.historyPlays[2] === this.historyPlays[8]) return true
+        // test for diagonal lanes
+        if(this.historyPlays[0] !== -1 && this.historyPlays[0] === this.historyPlays[4] && this.historyPlays[0] === this.historyPlays[8]) return true
         return false
     }
 }
